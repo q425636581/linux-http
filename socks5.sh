ebff10d6bb6edfb3e5daf5d087ca94ae58d9986d
q425636581 <126627520+q425636581@users.noreply.github.com>
2023-03-01 19:57:24 +0800

    Update socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+function install_socks5() {
+  systemctl start firewalld #打开防火墙
+  firewall-cmd --zone=public --add-port=10316/tcp --permanent #打开端口10316
+  firewall-cmd --reload #重启防火墙
+  wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install_proxy.sh
+  bash install_proxy.sh --port=10316 --user=liu --passwd=liu
+}
+install_http
+install_socks5
